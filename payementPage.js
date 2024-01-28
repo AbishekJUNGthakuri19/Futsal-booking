30f9e0f1b4caca5fe3f36b3972aaf06e382b713b
Abishek shahi <128833491+AbishekJUNGthakuri19@users.noreply.github.com>
2024-01-28 08:25:49 +0545

    Add files via upload

---
+++ payementPage.js
@@ -0,0 +1,30 @@
+const idInput = document.querySelector('.userid');
+const passwordInput = document.querySelector('.userpassword');
+
+const Login = document.querySelector('.login-btn');
+const Cancel = document.querySelector('.cancel-btn');
+
+const popup = document.getElementById('Popup');
+
+
+Login.addEventListener('click', () => {
+    if (idInput.value != '' && passwordInput.value != '') {
+        popup.classList.add('open-popup');
+        document.querySelector('.container').style.opacity = 0.4;
+    }
+    else {
+        alert('Please enter the esewa Id and Password.')
+    }
+
+});
+
+Cancel.addEventListener('click',()=>{
+    popup.classList.remove('open-popup');
+    window.location = 'userBookingPage.html';
+});
+
+function closePopup(){
+    window.location = 'userBookingPage.html';
+}
+
+
